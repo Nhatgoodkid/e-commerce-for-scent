99e6bf2448c074dd6f9779053130d1e887eb35a2
Dang Cong Nhat <dcn22402@gmail.com>
2023-07-28 08:43:38 +0700

    Install jazzmin for custom django admin

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
